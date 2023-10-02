5d5e6616062b712253265b5f656017c6d96520c8
leilalinoo <aleila0921@gmail.com>
2023-10-02 08:28:53 +0200

    commentek torlese

--- src/teszt20231002/Teszt20231002.java
+++ src/teszt20231002/Teszt20231002.java
@@ -1,20 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package teszt20231002;
 
-/**
- *
- * @author abraham.leila
- */
 public class Teszt20231002 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
+        
     }
     
 }
